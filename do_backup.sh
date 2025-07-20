1258350ef09c3715d3378bc28c6edc8d1412de7f
recomrad <vt@krdnet.ru>
2025-07-20 23:52:59 +0300

    Update do_backup.sh

--- do_backup.sh
+++ do_backup.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-CUSTOM_HOST_STRING=""
-if [ ! -z "$CUSTOM_HOST" ]; then
-    CUSTOM_HOST_STRING="--backup-id $CUSTOM_HOST"
-fi
-
-echo "------------------------------------------------------------"
-proxmox-backup-client backup $BACKUP_TARGETS $CUSTOM_HOST_STRING
-echo "------------------------------------------------------------"
+echo "DEFAULT EMPTY BACKUP JOB"
